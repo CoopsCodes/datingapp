b2b739da04d6233a707741d3d804e087d6b9782e
Cooper Viktor <im.cooperviktor@gmail.com>
2019-03-05 17:48:18 +1100

    cooper changes to profile prage

--- datingapp.rb
+++ datingapp.rb
@@ -17,10 +17,12 @@ class Datingapp
     end
 
     def profile_sign_in
+        puts("--------------------- Welcome to Easy Route to Love ---------------------")
         puts("Do you want to Log-In? to continue your quest for love and get matched to singles in your area")
         puts("Type 'login' for love")
         puts
         user_sign_in = gets().chomp.downcase
+        puts("--------------------- Welcome to Easy Route to Love ---------------------")
         case user_sign_in
         when "login"
             puts "Login Name:"
@@ -51,8 +53,12 @@ class Datingapp
     end
 
     def static_profile_page
+        time = Time.new
         puts("--------------------- Welcome to Easy Route to Love ---------------------")
         puts("Welcome: #{@name}")
+        puts("Today is #{time.ctime}")
+        puts
+        puts
         puts
         puts
         puts
@@ -60,10 +66,6 @@ class Datingapp
         puts
         puts
         puts("--------------------- Welcome to Easy Route to Love ---------------------")
-    end
-
-    def to_string()
-        "New profile: #{@name}, #{@age}, #{@sex}"
     end
 
     def reset_password
@@ -124,6 +126,6 @@ profile1.password = 'Password1'
 
 # p (profile1.get_gender)
 
-puts("--------------------- Welcome to Easy Route to Love ---------------------")
-puts
+# puts("--------------------- Welcome to Easy Route to Love ---------------------")
+# puts
 profile1.profile_sign_in
